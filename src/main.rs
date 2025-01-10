e611e2693d360eb1c0e9795e6d4a2ae0f137b2db
Kian_Bahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-01-09 21:55:18 -0500

    basic math and shutdown work

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,86 @@
 use std::env;
+use system_shutdown;
 
 fn main() {
   let mut args: Vec<String> = env::args().collect();
 
   if args.len() > 3 {
-    println!("Usage: calculator --shutdown(Optional) 'expression'");
+    println!("Usage: calculator --shutdown(Optional) <expression>");
+    println!("Dont use spaces. e.g. 3/(5*4)**2");
     println!("Supports: +, -, *, /. coming soon: %, ==, //, **");
     return;
   }
 
   let shutdown: bool;
-  if &args[1] == "--shutdown" {
+  if args[1] == "--shutdown" {
     shutdown = true;
+    args.remove(1);
   } else {
     shutdown = false;
   }
   
-  let mut expression: Vec<&str> = args[2].split(' ').collect();
-  let result: i32 = calculate(expression, shutdown);
+  let expression: Expression = string_to_expression(&args[1]);
+
+  let result = expression.evaluate(shutdown);
   println!("Answer: {result}");
 }
 
-fn calculate(mut expression: Vec<&str>, shutdown: bool) -> i32 {
-  if expression.len() == 3 {
-    let operator: &str = expression[1];
-    let a: i32 = expression[0].trim().parse().expect("a is cooked");
-    let b: i32 = expression[2].trim().parse().expect("b is cooked");
-
-    if operator == "+" {
-      a + b
-    } else if operator == "-" {
-      a - b
-    } else if operator == "*" {
-      a * b
-    } else if operator == "/" {
-      if b == 0 && shutdown {
-        panic!("todo, implement shutdown thingy");
-      } else if b == 0 {
-        panic!("Add --shutdown flag to shutdown on division by zero");
+fn string_to_expression(string: &str) -> Expression {
+  for (i, chr) in string.chars().enumerate() {
+    match chr {
+      '+' => {
+        let a = string_to_expression(&string[0..i]);
+        let b = string_to_expression(&string[1+i..]);
+        return Expression::Add(Box::new(a), Box::new(b));
       }
-      a / b
-    } else {
-      panic!("Operation not supported");
+      '-' => {
+        let a = string_to_expression(&string[0..i]);
+        let b = string_to_expression(&string[1+i..]);
+        return Expression::Sub(Box::new(a), Box::new(b));
+      }
+      '*' => {
+        let a = string_to_expression(&string[0..i]);
+        let b = string_to_expression(&string[1+i..]);
+        return Expression::Mult(Box::new(a), Box::new(b));
+      }
+      '/' => {
+        let a = string_to_expression(&string[0..i]);
+        let b = string_to_expression(&string[1+i..]);
+        return Expression::Div(Box::new(a), Box::new(b));
+      }
+      _ => continue,
     }
-  } else if expression.len() > 3 {
-    dbg!(expression);
-    panic!("Todo, support complex equations");
-  } else {
-    panic!("Bro wtf");
+  }
+  Expression::Simple(string.parse().expect("bombaclaat"))
+}
+
+enum Expression {
+  Simple(f32),
+  Add(Box<Expression>, Box<Expression>),
+  Sub(Box<Expression>, Box<Expression>),
+  Mult(Box<Expression>, Box<Expression>),
+  Div(Box<Expression>, Box<Expression>),
+}
+
+impl Expression {
+  fn evaluate(&self, shutdown: bool) -> f32 {
+    match self {
+      Expression::Simple(value) => *value,
+      Expression::Add(left, right) => left.evaluate(shutdown) + right.evaluate(shutdown),
+      Expression::Sub(left, right) => left.evaluate(shutdown) - right.evaluate(shutdown),
+      Expression::Mult(left, right) => left.evaluate(shutdown) * right.evaluate(shutdown),
+      Expression::Div(left, right) => {
+        let b = right.evaluate(shutdown);
+        if b == 0.0 && shutdown { // shutdown not found in this scope :cry:
+          match system_shutdown::shutdown() {
+            Ok(_) => println!("division by 0, shutting down"),
+            Err(error) => eprintln!("Failed to shut down: {}", error),    
+          }
+        } else if b == 0.0 {
+          panic!("You cant divide by 0, silly!!!!");
+        }
+        left.evaluate(shutdown) / right.evaluate(shutdown)
+      }
+    }
   }
 }
